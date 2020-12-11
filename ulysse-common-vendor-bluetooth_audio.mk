bbb295b7f119c8f1e091dccdb2416efae90f606e
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2020-12-10 20:37:34 -0500

    ulysse-common: Add missing passthrough impl for btconfigstore
    * Fixes:
      E android.hardware.bluetooth@1.0-service-qti: Could not get passthrough
      implementation for vendor.qti.hardware.btconfigstore@1.0::IBTConfigStore/default.
    
    Change-Id: I7e103d8bfbb302bda37847871278e45bf0bd1a7f

--- ulysse-common-vendor-bluetooth_audio.mk
+++ ulysse-common-vendor-bluetooth_audio.mk
@@ -14,4 +14,5 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/ulysse-common/bluetooth_audio/vendor/lib64/libbluetooth_audio_session_qti.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libbluetooth_audio_session_qti.so \
     vendor/xiaomi/ulysse-common/bluetooth_audio/vendor/lib64/vendor.qti.hardware.bluetooth_audio@2.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.bluetooth_audio@2.0.so \
     vendor/xiaomi/ulysse-common/bluetooth_audio/vendor/lib64/vendor.qti.hardware.bluetooth_sar@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.bluetooth_sar@1.0.so \
-    vendor/xiaomi/ulysse-common/bluetooth_audio/vendor/lib64/vendor.qti.hardware.btconfigstore@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.btconfigstore@1.0.so
+    vendor/xiaomi/ulysse-common/bluetooth_audio/vendor/lib64/vendor.qti.hardware.btconfigstore@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.btconfigstore@1.0.so \
+    vendor/xiaomi/ulysse-common/bluetooth_audio/vendor/lib64/hw/vendor.qti.hardware.btconfigstore@2.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/vendor.qti.hardware.btconfigstore@2.0-impl.so
