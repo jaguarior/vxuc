432d7d54b155e190d9154a5aab59686503c64233
R. Adi Permana <romadhonadipermana279f@gmail.com>
2021-03-15 21:08:26 +0700

    ulysse-common: Update Alarm blobs from LA.UM.9.6.2.r1-03600-89xx.0
    Co-authored-by: Jabiyeff <cebiyevanar@gmail.com>

--- ulysse-common-vendor-alarm.mk
+++ ulysse-common-vendor-alarm.mk
@@ -1,5 +1,4 @@
 PRODUCT_COPY_FILES += \
-    vendor/xiaomi/ulysse-common/alarm/product/framework/vendor.qti.hardware.alarm-V1.0-java.jar:$(TARGET_COPY_OUT_PRODUCT)/framework/vendor.qti.hardware.alarm-V1.0-java.jar \
     vendor/xiaomi/ulysse-common/alarm/vendor/bin/hw/vendor.qti.hardware.alarm@1.0-service:$(TARGET_COPY_OUT_VENDOR)/bin/hw/vendor.qti.hardware.alarm@1.0-service \
     vendor/xiaomi/ulysse-common/alarm/vendor/bin/power_off_alarm:$(TARGET_COPY_OUT_VENDOR)/bin/power_off_alarm \
     vendor/xiaomi/ulysse-common/alarm/vendor/etc/init/vendor.qti.hardware.alarm@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.alarm@1.0-service.rc \
